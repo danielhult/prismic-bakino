5874761602d9bb61ab7518edf5e5aaa3ecdbe35d
Daniel Hult <daniel.hult@appex.no>
2020-06-25 20:01:01 +0200

    Added gsap bonus files

--- assets/animations/Split.js
+++ assets/animations/Split.js
@@ -1,5 +1,5 @@
 import gsap from 'gsap';
-import { SplitText } from 'gsap';
+import SplitText from 'gsap/SplitText';
 gsap.registerPlugin(SplitText);
 
 export const lineSplit = lines => {
--- assets/animations/transitions.js
+++ assets/animations/transitions.js
@@ -1,5 +1,5 @@
 import gsap from 'gsap';
-//import ScrollToPlugin from 'gsap/scrollToPlugin';
+import ScrollToPlugin from 'gsap/scrollToPlugin';
 import { lineSplit, wordSplit } from './Split';
 import { wipeY, wipeFromY } from './Wipes';
 import {
@@ -29,7 +29,7 @@ export const page = {
         });
 
         master
-            //.to(window, { duration: 0.5, scrollTo: 0 })
+            .to(window, { duration: 0.5, scrollTo: 0 })
             .add(leaveTextAnimation(heroTitle.chars))
             .add(skewOutUp(heroText.lines), '<')
             .add(skewOutUp('.hero .btn'), '<')
