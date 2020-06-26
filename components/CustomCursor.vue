ce0c61f69fc4c41ea88ab6b68871613ccc71e761
Daniel Hult <daniel.hult@appex.no>
2020-06-26 12:13:48 +0200

    Added custom cursor

--- components/CustomCursor.vue
+++ components/CustomCursor.vue
@@ -1,56 +1,15 @@
 <template>
-    <div class="cursor"></div>
+    <div></div>
 </template>
 
 <script>
-import gsap from 'gsap';
+import kursor from 'kursor';
 
 export default {
     mounted() {
         if (process.client) {
-            let mouseIsDown = false;
-
-            window.addEventListener('mousedown', e => {
-                mouseIsDown = true;
-                gsap.to('.cursor', {
-                    duration: 0.2,
-                    scale: 0.3,
-                });
-            });
-
-            window.addEventListener('mouseup', e => {
-                mouseIsDown = false;
-                gsap.to('.cursor', {
-                    duration: 0.2,
-                    scale: 1,
-                });
-            });
-
-            const customCursor = e => {
-                gsap.to('.cursor', {
-                    x: e.clientX,
-                    y: e.clientY,
-                });
-            };
-
-            const growMouse = e => {
-                gsap.to('.cursor', {
-                    scale: 1.5,
-                });
-            };
-
-            const shrinkMouse = e => {
-                gsap.to('.cursor', {
-                    scale: 1,
-                });
-            };
-
-            const buttons = document.querySelectorAll('button .btn');
-
-            window.addEventListener('mousemove', e => customCursor(e));
-            buttons.forEach(button => {
-                button.addEventListener('mouseover', e => growMouse(e));
-                button.addEventListener('mouseleave', e => shrinkMouse(e));
+            new kursor({
+                type: 1,
             });
         }
     },
